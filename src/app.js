19e5a98e7e3aa415b439de7040c3b5bff19c733e
andersontbernal <andersonbernal7@gmail.com>
2025-06-09 02:55:32 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
 import "./style.css";
 
+window.onload = () => {
+  const card = document.querySelector(".card");
+  const number = card.querySelector(".number");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const suit = generateRandomSuit();
+  const value = generateRandomNumber();
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  card.classList.add(suit);
+  number.textContent = value;
+};
+
+const generateRandomNumber = () => {
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  return numbers[Math.floor(Math.random() * numbers.length)];
+};
+
+const generateRandomSuit = () => {
+  const suits = ["diamond", "spade", "heart", "club"];
+  return suits[Math.floor(Math.random() * suits.length)];
 };
